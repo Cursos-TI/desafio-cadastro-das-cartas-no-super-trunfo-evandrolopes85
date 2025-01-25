3aa8a6385bce6d0e1413d064e45ccbf6c4bc287d
Evandro Lopes <52211865+evandrolopes85@users.noreply.github.com>
2025-01-25 18:54:39 +0000

    Exibindo dados do carta para o usuario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,9 +11,6 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
     // Declaração de Variáveis da primeira carta
-    char estado; // declara váriavel estado do tipo char
-    char codigoDaCarta[3];       // declara váriavel codigoDaCarta do tipo char com 3 caracteres
-    char nomeDacidade[100];      // declara váriavel nomeDacidade do tipo char com até 100 caracteres
     int populacao;               // declara váriavel populacao do tipo inteiro
     float area;                  // declara váriavel area do tipo ponto flutuante
     float pib;                   // declara váriavel pib do tipo ponto flutuante
@@ -22,36 +19,25 @@ int main() {
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    printf("Digite o estado, sendo uma letra entre A e H: \n");
-    scanf("%c", &estado);
+    printf("Digite a população: \n"); // Mensagem pedindo para o usuario digitar a população da cidade
+    scanf("%d", &populacao);          // Armazenando o dado digitado pelo usuario na várialvel populacao
 
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", nomeDacidade);
+    printf("Digite a area da cidade: \n"); // Mensagem pedindo para o usuario digitar a area da cidade
+    scanf("%f", &area);                    // Armazenando o dado digitado pelo usuario na várialvel area
 
-    printf("Digite a população: \n");
-    scanf("%d", &populacao);
+    printf("Digite o pib (Produto Interno Bruto) da cidade: \n"); // Mensagem pedindo para o usuario digitar o PIB da cidade
+    scanf("%f", &pib);                                            // Armazenando o dado digitado pelo usuario na várialvel pib
 
-    printf("Digite a area da cidade: \n");
-    scanf("%f", &area);
-
-    printf("Digite o pib (Produto Interno Bruto) da cidade: \n");
-    scanf("%f", &pib);
-
-    printf("Digite o número de pontos turisticos: \n");
-    scanf("%d", &numeroDePontoTuristicos);
-
-    printf("Eatado: %c \n", estado);
-    printf("Nome da cidade: %s \n", nomeDacidade);
-    printf("População: %d \n", populacao);
-    printf("Área: %.2f \n", area);
-    printf("PIB: %.2f \n", pib);
-    printf("Nome da cidade: %d \n", numeroDePontoTuristicos);
-
-    // Cadastro da primeira carta
+    printf("Digite o número de pontos turisticos: \n");  // Mensagem pedindo para o usuario digitar o número de pontos turisticos da cidade
+    scanf("%d", &numeroDePontoTuristicos);               // Armazenando o dado digitado pelo usuario na várialvel numeroDePontoTuristicos
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    printf("População: %d \n", populacao); // exibindo a população da cidade
+    printf("Área: %.2f Km2 \n", area); // exibindo área com 2 casas decimais
+    printf("PIB: %.2f bilhões de reais\n", pib); // exibindo PIB com 2 casas decimais
+    printf("Número de Pontos Turísticos: %d \n", numeroDePontoTuristicos);  // exibindo a quantidade de ponto turistico da cidade
     return 0;
 }
