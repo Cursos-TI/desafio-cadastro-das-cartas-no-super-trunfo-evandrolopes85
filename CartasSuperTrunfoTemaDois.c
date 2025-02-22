a8823ab3bdac0c9fbe5e439bdf98ac92935c1ce3
evandrolopes85 <evandrocwd@gmail.com>
2025-02-21 22:27:16 -0300

    feature: desafio novado do tema 2 comparando cartas com atributos de minha escolha, direto no codigo

--- CartasSuperTrunfoTemaDois.c
+++ CartasSuperTrunfoTemaDois.c
@@ -11,8 +11,8 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Declarando variaveis para 2 estados cada um com 1 carta
-    char estado1, estado2; 
-    char codigoDaCarta1[4], codigoDaCarta2[4];
+    char estado1[3], estado2[3]; 
+    char codigoDaCarta1[10], codigoDaCarta2[10];
     char nomeDaCidade1[50],nomeDaCidade2[50];
     unsigned long int populacao1, populacao2;
     float area1, area2;
@@ -27,28 +27,28 @@ int main() {
     printf("\n----- Jogo Super Trunfo - Tema 1 -----\n\n"); // Titulo do jogo
 
     // entrada de dados para o primeiro estado 
-    printf("Escolha uma letra entre A e H para o nome do estado: \n");
-    scanf("%c", &estado1);
+    printf("Digite a sigla do estado, ex. SP: \n");
+    scanf("%s", estado1);
 
-    printf("Cadastro da carta do estado: '%c'\n", estado1);
-    printf("Digite código da primeira carta.\nVocê escolheu o estado '%c', então digite o codigo entre %c01 e %c04.\n", estado1, estado1, estado1);
-    scanf("%s", codigoDaCarta1);
+    printf("Cadastro da carta do estado: '%s'\n", estado1);
+    printf("Digite código da primeira carta.\n");
+    scanf(" %s", codigoDaCarta1);
     getchar();
 
-    printf("Digite o nome da cidade:\n");
+    printf("Digite o nome da cidade do estado (%s):\n", estado1);
     fgets(nomeDaCidade1, 50, stdin); // lendo string com espaco em branco
     nomeDaCidade1[strcspn(nomeDaCidade1, "\n")] = 0; //remvendo o \n do nome da cidade
 
-    printf("Digite a população da cidade:\n");
+    printf("Digite a população da cidade (%s):\n", nomeDaCidade1);
     scanf("%lu", &populacao1);
 
-    printf("Digite a area da cidade: \n");
+    printf("Digite a area da cidade (%s): \n", nomeDaCidade1);
     scanf("%f", &area1);
 
-    printf("Digite o PIB da cidade em bilhões: \n");
+    printf("Digite o PIB da cidade (%s) em bilhões: \n", nomeDaCidade1);
     scanf("%f", &pib1);
 
-    printf("Digite o número de pontos turisticos: \n");
+    printf("Digite o número de pontos turisticos da (%s): \n", nomeDaCidade1);
     scanf("%d", &numeroDePontosTuristicos1);
 
     densidadePopulacional1 = (float)populacao1 / area1;
@@ -57,29 +57,29 @@ int main() {
     superPoder1 = (float)populacao1 + area1 + pib1 + (float)numeroDePontosTuristicos1 + pibPerCapita1 + (float)(1/densidadePopulacional1); // 1 / densidadePopulacional1 calcula o inverso da densidade populacionasl
 
     // entrada de dados para o segundo estado 
-    printf("\nEscolha uma letra entre A e H para o nome do novo estado, que seja diferente do anterior. \n");
-    scanf(" %c", &estado2);
+    printf("\nCadastro do novo estado.\nDigite a sigla do estado, diferente do anterior ex.  RJ: \n");
+    scanf(" %s", estado2);
+    //getchar();
+
+    printf("Cadastro da carta do estado: '%s'\n", estado2);
+    printf("Digite código da primeira carta. do estado %s\n",estado2);
+    scanf(" %s", codigoDaCarta2);
     getchar();
 
-    printf("Cadastro da carta do estado: '%c'\n", estado2);
-    printf("Digite código da primeira carta.\nVocê escolheu o estado '%c', então digite o codigo entre %c01 e %c04.\n", estado2, estado2, estado2);
-    scanf("%s", codigoDaCarta2);
-    getchar();
-
-    printf("Digite o nome da cidade:\n");
+    printf("Digite o nome da cidade do estado do (%s):\n", estado2);
     fgets(nomeDaCidade2, 50, stdin); // lendo string com espaco em branco
     nomeDaCidade2[strcspn(nomeDaCidade2, "\n")] = 0; //remvendo o \n do nome da cidade
 
-    printf("Digite a população da cidade:\n");
+    printf("Digite a população da cidade (%s):\n", nomeDaCidade2);
     scanf("%lu", &populacao2);
 
-    printf("Digite a area da cidade: \n");
+    printf("Digite a area da cidade (%s): \n", nomeDaCidade2);
     scanf("%f", &area2);
 
-    printf("Digite o PIB da cidade em bilhões: \n");
+    printf("Digite o PIB da cidade (%s) em bilhões: \n", nomeDaCidade2);
     scanf("%f", &pib2);
 
-    printf("Digite o número de pontos turisticos: \n");
+    printf("Digite o número de pontos turisticos da cidade (%s): \n", nomeDaCidade2);
     scanf("%d", &numeroDePontosTuristicos2);
 
     densidadePopulacional2 = (float)populacao2 / area2;
@@ -94,7 +94,7 @@ int main() {
    // exibindo valores comentados das cartas
    printf("\nExibindo cartas cadastradas\n");
    printf("\nCarta 1:");
-   printf("\nEstado: %c\n", estado1);
+   printf("\nEstado: %s\n", estado1);
    printf("Código da Carta: %s\n", codigoDaCarta1);
    printf("Nome da Cidade: %s\n", nomeDaCidade1);
    printf("Populacao: %lu\n", populacao1);
@@ -105,7 +105,7 @@ int main() {
    printf("PIB per Capita: %.2f reais \n", pibPerCapita1);
 
    printf("\nCarta 2:");
-   printf("\nEstado: %c\n", estado2);
+   printf("\nEstado: %s\n", estado2);
    printf("Código da Carta: %s\n", codigoDaCarta2);
    printf("Nome da Cidade: %s\n", nomeDaCidade2);
    printf("Populacao: %lu\n", populacao2);
@@ -118,14 +118,19 @@ int main() {
    // Exibindo resultados das comparações
    // verifico qual carta ganhou utilizando o operador relacional  >(maior que), com cada atributo
    // se o atributo for maior retorna 1 se não 0 
-   printf("\nComparação de Cartas:\n");
-   printf("Populacao: Carta 1 (%d)\n", (populacao1 > populacao2)); 
-   printf("Área: Carta 1 venceu (%d):\n",(area1 > area2));
-   printf("PIB: Carta 1 venceu (%d)\n", (pib1 > pib2));
-   printf("Pontos Turísticos: Carta 1 venceu (%d)\n", (numeroDePontosTuristicos1 > numeroDePontosTuristicos2));
-   printf("Densidade Populacioal: Carta 1 venceu (%d)\n", (densidadePopulacional1 < densidadePopulacional2));
-   printf("PIB per Capita: Carta 1 venceu (%d):\n", (pib1 > pib2));
-   printf("Super Poder: Carta 1 venceu (%d)\n", (superPoder1 > superPoder2));
+
+   printf("\nComparação de Cartas (Atribudo: Populacao):\n");
+   // Comparando as cartas pelo atributo Populacao
+   // Se a populacao da carta 1 for maior que o atributo da carta 2 então carta 1 e exibe as informacoes da carta,atributo escolhido e a carta1 ganhadora
+   if(populacao1 > populacao2){
+      printf("Carta 1 - %s (%s): %lu\n", nomeDaCidade1, estado1, populacao1);
+      printf("Carta 2 - %s (%s): %lu\n", nomeDaCidade2, estado2, populacao2);
+      printf("Resultado: Carta 1 (%s) venceu!\n", nomeDaCidade1);
+   }else{ // se não carta 2 ganhou e exibe as informacoes da carta,atributo escolhido e a carta2 ganhadora
+      printf("Carta 1 - %s (%s): %lu\n", nomeDaCidade1, estado1, populacao1);
+      printf("Carta 2 - %s (%s): %lu\n", nomeDaCidade2, estado2, populacao2);
+      printf("Resultado: Carta 2 (%s) venceu!\n", nomeDaCidade2);
+   }
 
    return 0;
 }
